769d5c9cfc69c2b5ce2200857e57c63c17b15577
Juan Cruz <jc23729@gmail.com>
2021-02-07 10:35:24 -0500

    update

--- app.py
+++ app.py
@@ -26,3 +26,70 @@ toolbar = DebugToolbarExtension(app)
 
 
 ##############################################################################
+"""Step 2: Make Homepage Listing Pets
+The homepage (at route /) should list the pets:
+>>>So at this point I create route @app.route that renders or creates the html page in the template folder, 
+name
+show photo, if present
+display “Available” in bold if the pet is available for adoption"""
+
+
+@app.route("/")
+def list_pets():
+    """List all pets."""
+
+    pets =Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
+
+
+"""Create route for Step 3
+Add Pet Form This should be at the URL path /add so we created the @app.route("/add", methods=["GET", "POST"])
+. Add a link to this from the homepage."""
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """Add a pet."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        new_pet = Pet(**data)
+        # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{new_pet.name} added.")
+        return redirect(url_for('list_pets'))
+
+    else:
+        # re-present form for editing
+        return render_template("pet_add_form.html", form=form)
+
+
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def edit_pet(pet_id):
+    """Edit pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+        db.session.commit()
+        flash(f"{pet.name} updated.")
+        return redirect(url_for('list_pets'))
+
+    else:
+        # failed; re-present form for editing
+        return render_template("pet_edit_form.html", form=form, pet=pet)
+
+
+@app.route("/api/pets/<int:pet_id>", methods=['GET'])
+def api_get_pet(pet_id):
+    """Return basic info about pet in JSON."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    info = {"name": pet.name, "age": pet.age}
+
+    return jsonify(info)
---
+++ forms.py
@@ -0,0 +1,24 @@
+"""Forms for adopt app."""
+#Imports for forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets"""
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()],
+    )
+    species = SelectField(
+        "Species",
+        choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")],
+    )
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+    
+   
+    
